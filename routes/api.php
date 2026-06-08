a0a0730e9ad337fee4159073aacc5eb60bf34808
rajivun <rajivcandra01@gmail.com>
2026-06-08 15:34:25 +0700

    Add files via upload

---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\KamarController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\RentalController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/kamar', [KamarController::class, 'index']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/profile', [AuthController::class, 'profile']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/rentals', [RentalController::class, 'index']);
+
+    Route::post('/rentals', [RentalController::class, 'store']);
+
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
